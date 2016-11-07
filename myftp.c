62e8250bfe3585ca6b12adddcfbafd339f1378c0
Matthew Reilly <matt@matthews-air.dhcp.nd.edu>
2016-11-07 16:05:09 -0500

    Bare bones from last assignment

---
+++ myftp.c
@@ -0,0 +1,114 @@
+/* TCP/UDP MessageBoardForum Client
+ * Emily Obaditch - eobaditc
+ * Teddy Brombach - tbrombac
+ * Matt Reilly - mreill11
+ *
+ * This is our implementation of a Simple FTP Client, written in C.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mhash.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+
+#define BUFSIZE 4096
+
+void readFile(char *dest, char *fname);
+void error(char *msg);
+
+int main(int argc, char *argv[]) {
+    int XIT = 0;
+    MHASH td; 
+    char filecontent[BUFSIZE]; 
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
+    int sockfd, filesize, portno, n, k, serverlen;
+    char *hostname;
+    char *command;
+    unsigned char * serverHash; 
+    char name[BUFSIZE]; 
+    char len [BUFSIZE]; 
+    char buf[BUFSIZE];
+    int len_s; 
+    char key[BUFSIZE];
+    int i = 0;
+
+    /* check command line arguments */
+    // CHANGE <text or file name>
+    if (argc != 3) {
+        fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+        exit(0);
+    }
+    hostname = argv[1];
+    portno = atoi(argv[2]);
+
+
+    // Load buffer, 
+
+    bzero(buf, BUFSIZE);
+    // Create the socket 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0){
+        error("ERROR opening socket");
+    }
+    // Load DNS Entry
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+        exit(0);
+    }
+
+    // Obtain server address
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(portno);
+
+    /* connect */
+    if (connect(sockfd,(struct sockaddr*) &serveraddr, sizeof(serveraddr)) < 0){
+        error ("ERROR connecting"); 
+    }
+
+    while(1){
+        printf("Enter Command: "); 
+        scanf("%s", buf);  
+        n = write(sockfd,buf, strlen(buf)); 
+        if(n<0)
+            error("ERROR writing to socket");
+
+        bzero(buf, BUFSIZE); 
+    }
+
+    return 0; 
+}
+
+// Read file into buffer
+void readFile(char *dest, char *fname) {
+    FILE *fp = fopen(fname, "r");
+    if (fp != NULL) {
+        size_t new_len = fread(dest, sizeof(char), BUFSIZE, fp);
+        if (ferror(fp) != 0) {
+            fputs("Error reading file", stderr);
+        } else {
+            dest[new_len++] = '\0';
+        }
+        fclose(fp);
+    }
+}
+
+// error handling
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
---
+++ myftpd.c
@@ -0,0 +1,150 @@
+/* TCP/UDP MessageBoardForum Server
+ * Emily Obaditch - eobaditc
+ * Teddy Brombach - tbrombac
+ * Matt Reilly - mreill11
+ *
+ * This is our implementation of a Simple FTP Server, written in C.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <mhash.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
+
+#define BUFSIZE 4096
+
+void readFile(char *dest, char*fname);
+int path_is_directory (const char* path);
+
+// error handling
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd; // socket 
+    int sockfd2;
+    MHASH td; 
+    int port; // port number
+    int clientlen; // byte size of client's address
+    struct sockaddr_in serveraddr; // server's addr
+    struct sockaddr_in clientaddr; // client addr
+    struct timeval start_t, end_t; 
+    struct hostent *hostp; // client host info
+    char buf[BUFSIZE]; // message buffer
+    char *hostaddrp; // dotted decimal host addr string
+    int optval; // flag value for setsockopt
+    int n, k; // n = message size, k = key size
+    int i;    // counter
+    short len;
+    char *name;
+    char *len_string; 
+    unsigned char * serverHash; 
+    char com[BUFSIZE];
+    char* path;
+    char choice[BUFSIZE]; 
+    char* filename;
+
+    // parse command line arguments
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port> <encryption key>\n", argv[0]);
+        exit(1);
+    }
+
+    port = atoi(argv[1]);
+
+    // create the parent socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    // setsockopt: rerun server faster
+    optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+
+    // build server internet address
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)port);
+
+    // bind the parent socket and port
+    if (bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
+        error("ERROR on binding");
+
+    //Listen 
+    while(1){
+
+        if (listen(sockfd, 5) < 0)
+            error("Error on binding");
+
+        // Wait for message, send response
+        clientlen = sizeof(clientaddr);
+    
+        sockfd2 = accept( sockfd, (struct sockaddr *) &clientaddr, &clientlen);
+        if(sockfd2 < 0)
+            error("Error on accept");
+        
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+
+        if (hostp == NULL)
+            error("ERROR on gethostbyaddr");
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL)
+            error("ERROR on inet_ntoa\n");
+
+        printf("Server connected with %s (%s)\n", hostp->h_name, hostaddrp);
+
+        while(1) {
+            // receive a datagram from a client
+            bzero(buf, BUFSIZE);
+            n = read(sockfd2, buf, BUFSIZE);
+            strcpy(com,buf);
+
+            if(n < 0)
+               error("Error reading from socket");0
+        
+            n = write(sockfd2, buf, strlen(buf));
+
+            if(n < 0)
+                error("Error reading from socket");
+
+        }
+    }
+}
+
+void readFile(char *dest, char *fname){
+    FILE *fp = fopen(fname, "r");
+    if(fp != NULL){
+        size_t new_len = fread(dest, sizeof(char), BUFSIZE, fp);
+        if(ferror(fp) != 0){
+            fputs("error reading file" , stderr);
+        } else{
+            dest[new_len++] = '\0';
+        }
+        fclose(fp);
+    }
+}
+
+int path_is_directory (const char* path) {
+    struct stat s_buf;
+
+    if (stat(path, &s_buf))
+        return 0;
+
+    return S_ISDIR(s_buf.st_mode);
+}
